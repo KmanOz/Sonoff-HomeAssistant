d6d4fad9e96b15c8ef26000d3f284ff6c2ec0102
KmanOz <pkyrkos@gmail.com>
2017-02-08 15:54:45 +1100

    Added v1.01 OTA Firmware

---
+++ arduino/ESPsonoff_POW-v1.01OTA/power.h
@@ -0,0 +1,102 @@
+/*
+  This libary is an enhanced version of the ITEAD.cc library
+
+  It reads the values from the Sonoff POW HLW8012 chip and calculates the respective
+  Power, current, and voltage values. Only voltage or current reading can be chosen.
+
+  Copyright (c) [2016] [Iteand.cc and Andreas Spiess]
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+#ifndef __POWER__H_
+#define __POWER__H_
+
+#include <functional>
+
+extern "C" {
+#include "user_interface.h"
+}
+
+typedef struct _DEVPARAM{
+    double param_a;
+    double param_b;
+}DEVPARAM;
+
+typedef enum{
+    CURRENT,
+    VOLTAGE,
+}MEASURETYPE;
+
+typedef enum
+{
+    IOTGO_HLW8012_TEST_MODE_REPEAT,    
+    IOTGO_HLW8012_TEST_MODE_ONCE,
+}TESTMODE;
+
+class ESP8266PowerClass{
+    public:
+        ESP8266PowerClass();
+        ESP8266PowerClass(int8_t power_pin,int8_t current_voltage_pin,int8_t sel_pin);
+        void enableMeasurePower(void);
+        void selectMeasureCurrentOrVoltage(MEASURETYPE dev_type);
+        void setPowerParam(double param_a,double param_b);
+        void setCurrentParam(double param_a,double param_b);
+        void setVoltageParam(double param_a,double param_b);
+        DEVPARAM getPowerParam(void);
+        DEVPARAM getCurrentParam(void);
+        DEVPARAM getvoltageParam(void);
+        void startMeasure(void);
+        double getPower(void);
+        double getCurrent(void);
+        double getVoltage(void);
+        double getCurrFrequency(void);
+    private:
+        static void measurePowerFreq(void);
+        static void measureCurrenFreq(void);
+        static void measureVoltageFreq(void);
+        static void timerCallback(void);
+        static void getFreq(uint32_t &cnt,uint32_t &freq_cnt,uint8_t &test_mode,uint32_t &freq,
+                                            uint8_t &hlw8012_bad_once_cnt,uint8_t &hlw8012_bad_repeat_cnt,uint32_t (&history_queue)[10]);
+        static void getAverageFreq(uint32_t (&history_queue)[10],uint32_t &freq);
+        static uint32_t power_freq_cnt;
+        static uint32_t voltage_freq_cnt;
+        static uint32_t current_freq_cnt;
+        static uint8_t power_test_mode;
+        static uint8_t current_test_mode;
+        static uint8_t voltage_test_mode;
+        static bool power_flag;
+        static bool current_flag;
+        static bool voltage_flag;
+        static uint32_t power_freq;
+        static uint32_t voltage_freq;
+        static uint32_t current_freq;
+        DEVPARAM power_param;
+        DEVPARAM current_param;
+        DEVPARAM voltage_param;
+        int8_t power_pin;
+        int8_t current_voltage_pin;
+        int8_t sel_pin;
+        
+        os_timer_t myTimer;
+        
+};
+
+#endif
+
---
+++ arduino/ESPsonoff_TH-v1.01pOTA/ESPsonoff_TH-v1.01pOTA.ino
@@ -0,0 +1,268 @@
+/*
+  Copyright (c) 2017 @KmanOz
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+  ==============================================================================
+  Changes in v1.01pOTA
+  
+    - Relay state now stored in EEPROM and will power up with last relay state
+    - Support for Wallswitch to turn the Sonoff ON & OFF on Pin 14.
+    - OTA Firmware Upgradable
+  ==============================================================================
+*/
+
+#include <EEPROM.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <PubSubClient.h>
+#include <Ticker.h>
+
+#define BUTTON          0                                    // (Don't Change for Sonoff TH Series)
+#define RELAY           12                                   // (Don't Change for Sonoff TH Series)
+#define LED             13                                   // (Don't Change for Sonoff TH Series)
+#define WALLSWITCH      14                                   // (Don't Change for Sonoff TH Series)
+
+#define MQTT_CLIENT     "Sonoff_Living_Room_v1.01pOTA"       // mqtt client_id (Must be unique for each Sonoff)
+#define MQTT_SERVER     "192.168.0.100"                      // mqtt server
+#define MQTT_PORT       1883                                 // mqtt port
+#define MQTT_TOPIC      "home/sonoff/living_room/1"          // mqtt topic (Must be unique for each Sonoff)
+#define MQTT_USER       "user"                               // mqtt user
+#define MQTT_PASS       "pass"                               // mqtt password
+
+#define WIFI_SSID       "homewifi"                           // wifi ssid
+#define WIFI_PASS       "homepass"                           // wifi password
+
+#define VERSION    "\n\n---------------  Sonoff TH Powerpoint v1.01pOTA  ---------------"
+
+bool rememberRelayState = true;                              // If 'true' remembers the state of the relay before power loss.
+bool OTAupdate = false;                                      // (Do not Change)
+bool requestRestart = false;                                 // (Do not Change)
+bool sendStatus = false;                                     // (Do not Change)
+
+int kUpdFreq = 1;                                            // Update frequency in Mintes to check for mqtt connection
+int kRetries = 10;                                           // WiFi retry count. Increase if not connecting to router.
+int wallSwitch = 1;                                          // (Do not Change)
+int lastWallSwitch = 1;                                      // (Do not Change)
+int lastRelayState;                                          // (Do not Change)
+
+unsigned long TTasks;                                        // (Do not Change)
+unsigned long count = 0;                                     // (Do not Change)
+
+extern "C" { 
+  #include "user_interface.h" 
+}
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient, MQTT_SERVER, MQTT_PORT);
+Ticker btn_timer;
+
+void callback(const MQTT::Publish& pub) {
+  if (pub.payload_string() == "stat") {
+  }
+  else if (pub.payload_string() == "on") {
+    digitalWrite(RELAY, HIGH);
+  }
+  else if (pub.payload_string() == "off") {
+    digitalWrite(RELAY, LOW);
+  }
+  else if (pub.payload_string() == "reset") {
+    requestRestart = true;
+  }
+  sendStatus = true;
+}
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(RELAY, OUTPUT);
+  pinMode(BUTTON, INPUT);
+  pinMode(WALLSWITCH, INPUT);
+  digitalWrite(LED, HIGH);
+  digitalWrite(RELAY, LOW);
+  Serial.begin(115200);
+  EEPROM.begin(8);
+  lastRelayState = EEPROM.read(0);
+  if (rememberRelayState && lastRelayState == 1) {
+     digitalWrite(RELAY, HIGH);
+  }
+  btn_timer.attach(0.05, button);
+  mqttClient.set_callback(callback);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  ArduinoOTA.onStart([]() {
+    OTAupdate = true;
+    blinkLED(LED, 400, 2);
+    digitalWrite(LED, HIGH);
+    Serial.println("OTA Update Initiated . . .");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nOTA Update Ended . . .s");
+    OTAupdate = false;
+    requestRestart = true;
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    digitalWrite(LED, LOW);
+    delay(5);
+    digitalWrite(LED, HIGH);
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    blinkLED(LED, 40, 2);
+    OTAupdate = false;
+    Serial.printf("OTA Error [%u] ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println(". . . . . . . . . . . . . . . Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println(". . . . . . . . . . . . . . . Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println(". . . . . . . . . . . . . . . Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println(". . . . . . . . . . . . . . . Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println(". . . . . . . . . . . . . . . End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println(VERSION);
+  Serial.print("\nUnit ID: ");
+  Serial.print("esp8266-");
+  Serial.print(ESP.getChipId(), HEX);;
+  Serial.print("\nConnecting to "); Serial.print(WIFI_SSID); Serial.print(" Wifi"); 
+  while ((WiFi.status() != WL_CONNECTED) && kRetries --) {
+    delay(500);
+    Serial.print(" .");
+  }
+  if (WiFi.status() == WL_CONNECTED) {  
+    Serial.println(" DONE");
+    Serial.print("IP Address is: "); Serial.println(WiFi.localIP());
+    Serial.print("Connecting to ");Serial.print(MQTT_SERVER);Serial.print(" Broker . .");
+    delay(500);
+    while (!mqttClient.connect(MQTT::Connect(MQTT_CLIENT).set_keepalive(90).set_auth(MQTT_USER, MQTT_PASS)) && kRetries --) {
+      Serial.print(" .");
+      delay(1000);
+    }
+    if(mqttClient.connected()) {
+      Serial.println(" DONE");
+      Serial.println("\n----------------------------  Logs  ----------------------------");
+      Serial.println();
+      mqttClient.subscribe(MQTT_TOPIC);
+      blinkLED(LED, 40, 8);
+      digitalWrite(LED, LOW);
+    }
+    else {
+      Serial.println(" FAILED!");
+      Serial.println("\n----------------------------------------------------------------");
+      Serial.println();
+    }
+  }
+  else {
+    Serial.println(" WiFi FAILED!");
+    Serial.println("\n----------------------------------------------------------------");
+    Serial.println();
+  }
+}
+
+void loop() { 
+  ArduinoOTA.handle();
+  if (OTAupdate == false) { 
+    mqttClient.loop();
+    timedTasks();
+    checkStatus();
+  }
+}
+
+void blinkLED(int pin, int duration, int n) {             
+  for(int i=0; i<n; i++)  {  
+    digitalWrite(pin, HIGH);        
+    delay(duration);
+    digitalWrite(pin, LOW);
+    delay(duration);
+  }
+}
+
+void button() {
+  if (!digitalRead(BUTTON)) {
+    count++;
+  } 
+  else {
+    if (count > 1 && count <= 40) {   
+      digitalWrite(RELAY, !digitalRead(RELAY));
+      sendStatus = true;
+    } 
+    else if (count >40){
+      Serial.println("\n\nSonoff Rebooting . . . . . . . . Please Wait"); 
+      requestRestart = true;
+    } 
+    count=0;
+  }
+}
+
+void checkConnection() {
+  if (WiFi.status() == WL_CONNECTED)  {
+    if (mqttClient.connected()) {
+      Serial.println("mqtt broker connection . . . . . . . . . . OK");
+    } 
+    else {
+      Serial.println("mqtt broker connection . . . . . . . . . . LOST");
+      requestRestart = true;
+    }
+  }
+  else { 
+    Serial.println("WiFi connection . . . . . . . . . . LOST");
+    requestRestart = true;
+  }
+}
+
+void checkStatus() {
+  if (sendStatus) {
+    if(digitalRead(RELAY) == LOW)  {
+      if (rememberRelayState) {
+        EEPROM.write(0, 0);
+      }
+      mqttClient.publish(MQTT::Publish(MQTT_TOPIC"/stat", "off").set_retain().set_qos(1));
+      Serial.println("Relay . . . . . . . . . . . . . . . . . . OFF");
+    } else {
+      if (rememberRelayState) {
+        EEPROM.write(0, 1);
+      }
+      mqttClient.publish(MQTT::Publish(MQTT_TOPIC"/stat", "on").set_retain().set_qos(1));
+      Serial.println("Relay . . . . . . . . . . . . . . . . . . ON");
+    }
+    if (rememberRelayState) {
+      EEPROM.commit();
+    }
+    sendStatus = false;
+  }
+  if (requestRestart) {
+    blinkLED(LED, 400, 4);
+    ESP.restart();
+  }
+}
+
+void checkWallSwitch() {
+  wallSwitch = digitalRead(WALLSWITCH);
+  if (wallSwitch != lastWallSwitch) {
+    digitalWrite(RELAY, !digitalRead(RELAY));
+    sendStatus = true;
+  }
+  lastWallSwitch = wallSwitch;
+}
+
+void timedTasks() {
+  if ((millis() > TTasks + (kUpdFreq*60000)) || (millis() < TTasks)) { 
+    TTasks = millis();
+    checkConnection();
+  }
+}
